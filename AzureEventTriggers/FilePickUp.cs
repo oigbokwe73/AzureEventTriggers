3a7ee8756630383df7e17460d60190fe83de19d2
obinna <oigbokwe73@gmail.com>
2022-02-16 13:13:40 -0500

    Updates to the config and code

--- AzureEventTriggers/FilePickUp.cs
+++ AzureEventTriggers/FilePickUp.cs
@@ -7,15 +7,15 @@ using Xenhey.BPM.Core.Implementation;
 
 namespace AzureEventTriggers
 {
-    public static class Function1
+    public static class FilePickUp
     {
-        [FunctionName("FileParser")]
-        public static void Run([BlobTrigger("processed/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob, string name, ILogger log)
+        [FunctionName("FilePickUp")]
+        public static void Run([BlobTrigger("pickup/{name}", Connection = "AzureWebJobsStorage")] Stream myBlob, string name, ILogger log)
         {
             string ApiKeyName = "x-api-key";
             log.LogInformation("C# blob trigger function processed a request.");
             NameValueCollection nvc = new NameValueCollection();
-            nvc.Add(ApiKeyName, "43EFE991E8614CFB9EDECF1B0FDED37C");
+            nvc.Add(ApiKeyName, "43EFE991E8614CFB9EDECF1B0FDED37A");
             IOrchrestatorService orchrestatorService = new ManagedOrchestratorService(nvc);
             var processFiles = orchrestatorService.Run(myBlob);
         }
